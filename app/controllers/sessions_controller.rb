674844cc65627075d8270f99f049dfe15f53f357
Jaime Humberto <jhcruz@zoho.com>
2011-09-30 20:10:48 -0500

    hasta el video 904

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #handel successful sign in
+      sign_in user
+      redirect_to user
     end
   end
 
